7e66ba682292c6dd2572b5e99ca3fad41acf19e6
Roy Straub <roy.straub@codesquad.nl>
2020-06-27 10:17:52 +0200

    Added some loops

---
+++ loops.py
@@ -0,0 +1,16 @@
+def while_loop():
+    i = 1
+    while i <= 10:
+        print(i)
+        i += 1
+
+
+while_loop()
+
+
+def for_loop():
+    for i in range(11, 21):
+        print(i)
+
+
+for_loop()
