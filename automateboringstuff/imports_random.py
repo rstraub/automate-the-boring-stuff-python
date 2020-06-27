d2a63a7f6132f713a889a5c4a2c45edb31105585
Roy Straub <roy.straub@codesquad.nl>
2020-06-27 10:46:34 +0200

    Other import style

--- automateboringstuff/imports_random.py
+++ automateboringstuff/imports_random.py
@@ -1,9 +1,9 @@
-import random
+from random import randint
 
 
 def random_num(amount):
     for num in range(amount):
-        print(random.randint(1, 10))
+        print(randint(1, 10))
 
 
 random_num(10)
