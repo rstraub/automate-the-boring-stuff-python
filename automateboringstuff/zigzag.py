d29d396280ad02332a9b51f911eabc1ee6206947
Roy Straub <roy.straub@codesquad.nl>
2020-08-21 11:28:02 +0200

    No more globals

--- automateboringstuff/zigzag.py
+++ automateboringstuff/zigzag.py
@@ -1,24 +1,29 @@
 import sys
 import time
 
-indent = 0
-indentIncreasing = True
-maxIndent = 20
-minIndent = 0
-symbols = '*******'
 
-try:
-    while True:
-        print(' ' * indent, end='')
-        print(symbols)
-        time.sleep(0.1)
-        if indentIncreasing:
-            indent = indent + 1
-            if indent == maxIndent:
-                indentIncreasing = False
-        else:
-            indent = indent - 1
-            if indent == minIndent:
-                indentIncreasing = True
-except KeyboardInterrupt:
-    sys.exit()
+def zigzag():
+    indent = 0
+    indentIncreasing = True
+    maxIndent = 20
+    minIndent = 0
+    symbols = '*******'
+
+    try:
+        while True:
+            print(' ' * indent, end='')
+            print(symbols)
+            time.sleep(0.1)
+            if indentIncreasing:
+                indent = indent + 1
+                if indent == maxIndent:
+                    indentIncreasing = False
+            else:
+                indent = indent - 1
+                if indent == minIndent:
+                    indentIncreasing = True
+    except KeyboardInterrupt:
+        sys.exit()
+
+
+zigzag()
