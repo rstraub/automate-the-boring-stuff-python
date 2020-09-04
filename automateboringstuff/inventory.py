3b5a58b38249b4e75dfbf87a75ed46fe1fd0b519
Roy Straub <roy.straub@codesquad.nl>
2020-09-04 11:23:06 +0200

    format single item

--- automateboringstuff/inventory.py
+++ automateboringstuff/inventory.py
@@ -2,6 +2,12 @@ def display_inventory(inventory):
     total_items(inventory)
 
 
+def formatted_item(item):
+    item_amount = item[1]
+    item_name = item[0]
+    return f'{item_amount} {item_name}'
+
+
 def total_items(inventory):
     total_amount = 0
     for item in inventory:
--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,6 +1,6 @@
 import unittest
 
-from automateboringstuff.inventory import total_items
+from automateboringstuff.inventory import total_items, formatted_item
 
 
 class Inventory(unittest.TestCase):
@@ -16,6 +16,18 @@ class Inventory(unittest.TestCase):
         result = total_items(inventory)
         self.assertEqual('Total number of items: 1', result)
 
+    def test_should_give_total_items_of_two_given_inventory_with_multiple_items(self):
+        inventory = {
+            'arrow': 1,
+            'potions': 1
+        }
+        result = total_items(inventory)
+        self.assertEqual('Total number of items: 2', result)
+
+    def test_should_display_inventory_item_given_inventory_with_at_least_one_item(self):
+        item = ['arrow', 1]
+        result = formatted_item(item)
+        self.assertEqual('1 arrow', result)
 
 if __name__ == '__main__':
     unittest.main()
