991d948ceeb6170b2f59f4170f99352839ec53be
Roy Straub <roy.straub@codesquad.nl>
2020-09-04 11:10:30 +0200

    Inventory

---
+++ automateboringstuff/inventory.py
@@ -0,0 +1,6 @@
+def display_inventory(inventory):
+    total_items(inventory)
+
+
+def total_items(inventory):
+    return 'Total number of items: 0'
---
+++ tests/test_inventory.py
@@ -0,0 +1,14 @@
+import unittest
+
+from automateboringstuff.inventory import total_items
+
+
+class Inventory(unittest.TestCase):
+    def test_should_give_total_items_of_zero_given_an_empty_inventory(self):
+        inventory = {}
+        result = total_items(inventory)
+        self.assertEqual('Total number of items: 0', result)
+
+
+if __name__ == '__main__':
+    unittest.main()
